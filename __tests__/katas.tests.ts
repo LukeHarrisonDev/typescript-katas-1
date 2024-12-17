096380bb886624b13de73e45f286626e2190475f
BassManLuke <luke_haz@yahoo.co.uk>
2024-12-17 09:46:40 +0000

    add convertToCamelCase

--- __tests__/katas.tests.ts
+++ __tests__/katas.tests.ts
@@ -1,4 +1,4 @@
-import { areValuesEqual } from "../katas-a1"
+import { areValuesEqual, convertToCamelCase } from "../katas-a1"
 
 describe("areValuesEqual", () => {
     test("when passed 2 values that are not identical, returns false", () => {
@@ -9,5 +9,23 @@ describe("areValuesEqual", () => {
     })
     test("when passed 2 values of different data types, returns false", () => {
         expect(areValuesEqual("Hello", 2)).toBe(false)
+    })
+})
+
+describe("convertToCamelCase", () => {
+    test("when passed a single word, lowercase string, returns that string", () => {
+        expect(convertToCamelCase("hello")).toBe("hello")
+    })
+    test("when passed a single word, uppercase string, return the string in lowercase", () => {
+        expect(convertToCamelCase("HELLO")).toBe("hello")
+    })
+    test("when passed two words of lowercase, returns them together with the second words first letter capitalised", () => {
+        expect(convertToCamelCase("hello world")).toBe("helloWorld")
+    })
+    test("when passed two words of varying case, returns them together with only the second words first letter capitalised", () => {
+        expect(convertToCamelCase("hElLo wOrLd")).toBe("helloWorld")
+    })
+    test("when passed more than two words of varying case, returns them as camelCase", () => {
+        expect(convertToCamelCase("HeLlO WoRld i LoVe codinG")).toBe("helloWorldILoveCoding")
     })
 })
--- katas-a1.ts
+++ katas-a1.ts
@@ -1,3 +1,20 @@
 export function areValuesEqual(value1: any, value2: any): boolean {
     return value1 === value2
 }
+
+export function convertToCamelCase(string: string): string {
+
+    const stringAsArray = string.toLowerCase().split(" ")
+
+    if (stringAsArray.length === 1) {
+        return stringAsArray[0]
+    }
+
+    const arrayWithCorrectCasing = [stringAsArray[0]]
+    
+    for (let i = 1; i < stringAsArray.length; i++) {
+        arrayWithCorrectCasing.push(stringAsArray[i][0].toUpperCase() + stringAsArray[i].slice(1))
+    }
+    
+    return arrayWithCorrectCasing.join("")
+}
